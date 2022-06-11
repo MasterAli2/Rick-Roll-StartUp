e61113213fcc9d7cabce3fa702059e892e1c8d1d
MasterAli2 <88894545+MasterAli2@users.noreply.github.com>
2022-06-11 12:23:31 -0400

    Add files via upload

---
+++ RickRoll StartUp.bat
@@ -0,0 +1,5 @@
+echo off
+
+echo start https://www.youtube.com/watch?v=xvFZjo5PgG0>"C:\Users\Jav\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\sys134.bat"
+
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
