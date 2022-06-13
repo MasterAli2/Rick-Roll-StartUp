e1c329e9045786990e2d53c11616d42b09c51709
MasterAli2 <88894545+MasterAli2@users.noreply.github.com>
2022-06-13 19:44:02 -0400

    Delete RickRoll StartUp.bat

--- RickRoll StartUp.bat
+++
@@ -1,5 +0,0 @@
-echo off
-
-echo start https://www.youtube.com/watch?v=xvFZjo5PgG0>"C:\Users\Jav\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\sys134.bat"
-
-start https://www.youtube.com/watch?v=xvFZjo5PgG0
